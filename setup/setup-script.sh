14be901c753d4972dbae773236cee1bd9e58fd58
CodeTurtle <48864709+codeturtle00@users.noreply.github.com>
2022-10-09 13:52:36 -0400

    Update setup-script.sh

--- setup/setup-script.sh
+++ setup/setup-script.sh
@@ -7,7 +7,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 
 echo Installing docker...
 sh ./get-docker.sh
-sudo apt-get install docker-compose-plugin
 
 echo Adding user to new docker group...
 sudo groupadd -f docker
