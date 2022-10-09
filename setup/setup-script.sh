db541e5e78b8a4eeca5e5285013515240957d600
codeturtle00 <jackyy2000@hotmail.com>
2022-10-09 01:56:00 -0400

    upload setup script

---
+++ setup/setup-script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo Installing npm and node, follow prompts...
+sudo apt-get install npm
+
+echo Downloading official docker install script...
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+echo Installing docker...
+sh ./get-docker.sh
+
+echo Adding user to new docker group...
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker 
+
+echo Done!
