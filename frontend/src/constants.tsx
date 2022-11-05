5a910daae3ebe2b3f4b2336a0f5cd60b16df59b7
Jacky Yang <jackyy2000@hotmail.com>
2022-11-04 23:28:20 -0400

    fix typo in ip

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,5 +1,5 @@
 const LOCAL = false
-export const BACKEND_URL = LOCAL ? "http://localhost:3001" : "http://3.16.78.249:3001/";
+export const BACKEND_URL = LOCAL ? "http://localhost:3001" : "http://3.16.78.249:3001";
 export const INCIDENTS_PER_AIRCRAFT_API =
   BACKEND_URL + "/incidents-per-aircraft";
 export const DATAPOINTS_BY_YEAR_API = BACKEND_URL + "/datapoints-by-year/";
