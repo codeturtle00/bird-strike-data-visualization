61e40d963aae1052ab65948ee7140d16cd05534a
Jacky Yang <jackyy2000@hotmail.com>
2022-12-06 03:33:58 -0500

    small aesthetic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from "chart.js"
 import { useState } from "react"
 import Menu from "./components/Menu";
+import ReactTooltip from 'react-tooltip'
 
 ChartJS.defaults.color  = "#fff";
 
@@ -19,12 +20,14 @@ function App () {
       {showMenu || <button 
         onClick={() => setShowMenu(!showMenu)}
         className="toggleMenuButton"
+        data-tip="Toggle Menu"
         onMouseEnter={() => setHoveringButton(true)}
         onMouseLeave={() => setHoveringButton(false)}
         style={{paddingLeft: hoveringButton ? "20px" : "0px"}}
       >{">"}</button>}
       <Menu show={showMenu} hideFunc={()=>setShowMenu(false)}/>
       <Map />
+      <ReactTooltip />
       
     </div>
   )
