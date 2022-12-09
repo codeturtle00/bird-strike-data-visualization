a864c6bb0bb4188cc9eac7811b72c4afb5bb4070
CodeTurtle <48864709+codeturtle00@users.noreply.github.com>
2022-12-09 00:27:20 -0500

    fix setup script exiting before npm install

--- setup/setup-script.sh
+++ setup/setup-script.sh
@@ -8,12 +8,12 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 echo Installing docker...
 sh ./get-docker.sh
 
+echo Running npm install
+(cd ../frontend; npm install)
+
 echo Adding user to new docker group...
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
 newgrp docker 
-
-echo Running npm install
-(cd ../frontend; npm install)
 
 echo Done!
